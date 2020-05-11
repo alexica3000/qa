e217d0f7a348319c8878ac22ad5ea7f9cbb5fae9
alex <my@mail.com>
2020-05-11 16:22:22 +0300

    Add vote controls to question and answer

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,11 @@
+// import { library, icon } from '@fortawesome/fontawesome-svg-core'
+import fontawesome from '@fortawesome/fontawesome-free'
+import faCaretUp from '@fortawesome/free-solid-svg-icons/faCaretUp'
+import faCaretDown from '@fortawesome/free-solid-svg-icons/faCaretDown'
+import faStar from '@fortawesome/free-solid-svg-icons/faStar'
+import faCheck from '@fortawesome/free-solid-svg-icons/faCheck'
+
+fontawesome.library.add([faCaretUp, faCaretDown, faStar, faCheck]);
+
+// library.add(faCaretUp);
+// const caretUp = icon({ prefix: 'fas', iconName: 'faCaretUp' });
