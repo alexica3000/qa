d68d6f26e8090e9698835881490f0691c358296d
alex <my@mail.com>
2020-05-11 13:28:05 +0300

    Display answers

--- app/User.php
+++ app/User.php
@@ -52,4 +52,11 @@ class User extends Authenticatable
     {
         return $this->hasMany('Answer');
     }
+
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;
+        $size = 22;
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?&s=" . $size;
+    }
 }
