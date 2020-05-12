1c96c58030f24dbb9baee95d1c5b3e2a43ae0d69
alex <my@mail.com>
2020-05-12 13:37:44 +0300

    Implement many to many polymorphic relationship

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UsersQuestionsAnswersTableSeeder::class,
-            FavoritesTableSeeder::class
+            FavoritesTableSeeder::class,
+            VotablesTableSeeder::class
         ]);
     }
 }
